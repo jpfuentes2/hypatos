65110b2bb2ad806ec12db52dbe5483245c51f501
Jacques Fuentes <jpfuentes2@gmail.com>
2014-04-16 23:33:50 -0400

    Use h as namespace ref in test

--- test/hypatos/core_test.clj
+++ test/hypatos/core_test.clj
@@ -1,107 +1,107 @@
 (ns hypatos.core-test
   (:require [clojure.test :refer :all]
             [clojure.pprint :refer [pprint]]
-            [hypatos.core :as v]))
+            [hypatos.core :as h]))
 
-(def c (v/connect "http://127.0.0.1:4001"))
+(def c (h/connect "http://127.0.0.1:4001"))
 
 ; Delete all data before each test
-(use-fixtures :each #(do (v/delete-all! c nil) (%)))
+(use-fixtures :each #(do (h/delete-all! c nil) (%)))
 
 (deftest remap-keys-test
-  (is (= (v/remap-keys inc {1 2 3 4})
+  (is (= (h/remap-keys inc {1 2 3 4})
          {2 2 4 4})))
 
 (deftest key-encoding-test
   (testing "nil"
-    (is (= "" (v/encode-key nil))))
+    (is (= "" (h/encode-key nil))))
 
   (testing "simple strings"
-    (is (= "foo" (v/encode-key "foo"))))
+    (is (= "foo" (h/encode-key "foo"))))
 
   (testing "strings with slashes"
-    (is (= "foo/bar" (v/encode-key "foo/bar"))))
+    (is (= "foo/bar" (h/encode-key "foo/bar"))))
 
   (testing "unicode"
-    (is (= "%E2%88%B4/%E2%88%8E" (v/encode-key "∴/∎"))))
+    (is (= "%E2%88%B4/%E2%88%8E" (h/encode-key "∴/∎"))))
 
   (testing "keywords"
-    (is (= "foo" (v/encode-key :foo))))
+    (is (= "foo" (h/encode-key :foo))))
 
   (testing "symbols"
-    (is (= "foo" (v/encode-key 'foo))))
+    (is (= "foo" (h/encode-key 'foo))))
 
   (testing "sequences"
-    (is (= "foo"     (v/encode-key [:foo])))
-    (is (= "foo/bar" (v/encode-key [:foo :bar])))
-    (is (= "foo/bar" (v/encode-key '(:foo :bar))))
-    (is (= "foo/bar/baz" (v/encode-key ["foo/bar" "baz"])))))
+    (is (= "foo"     (h/encode-key [:foo])))
+    (is (= "foo/bar" (h/encode-key [:foo :bar])))
+    (is (= "foo/bar" (h/encode-key '(:foo :bar))))
+    (is (= "foo/bar/baz" (h/encode-key ["foo/bar" "baz"])))))
 
 (deftest reset-get-test
   (testing "a simple key"
-    (v/reset! c "test" "hi")
-    (is (= "hi" (v/get c "test"))))
+    (h/reset! c "test" "hi")
+    (is (= "hi" (h/get c "test"))))
 
   (testing "Paths and unicode"
-    (v/reset! c "∴/∎" "ℵ")
-    (is (= "ℵ" (v/get c ['∴ '∎])))))
+    (h/reset! c "∴/∎" "ℵ")
+    (is (= "ℵ" (h/get c ['∴ '∎])))))
 
 (deftest list-directory
-  (is (= (v/get c nil)
+  (is (= (h/get c nil)
          nil))
 
-  (v/reset! c "foo" 1)
-  (v/reset! c "bar" 2)
-  (v/reset! c [:folder :of :stuff] "hi")
+  (h/reset! c "foo" 1)
+  (h/reset! c "bar" 2)
+  (h/reset! c [:folder :of :stuff] "hi")
 
-  (is (= (v/get c nil)
+  (is (= (h/get c nil)
          {"foo"     "1"
           "bar"     "2"
           "folder"  nil}))
 
-  (is (= (v/get c nil {:recursive? true})
+  (is (= (h/get c nil {:recursive? true})
          {"foo" "1"
           "bar" "2"
           "folder" {"of" {"stuff" "hi"}}})))
 
 (deftest missing-values
-  (is (nil? (v/get c :nonexistent))))
+  (is (nil? (h/get c :nonexistent))))
 
 (deftest create-test!
   (let [r (str (rand))
-        k (v/create! c :rand r)]
+        k (h/create! c :rand r)]
     (is (re-find #"^/rand/\d+$" k))
-    (is (= r (v/get c k)))))
+    (is (= r (h/get c k)))))
 
 (deftest cas-test!
-  (v/reset! c :foo :init)
-  (is (false? (v/cas! c :foo :nope :next)))
-  (is (= ":init" (v/get c :foo)))
+  (h/reset! c :foo :init)
+  (is (false? (h/cas! c :foo :nope :next)))
+  (is (= ":init" (h/get c :foo)))
 
-  (v/cas! c :foo :init :next)
-  (is (= ":next" (v/get c :foo))))
+  (h/cas! c :foo :init :next)
+  (is (= ":next" (h/get c :foo))))
 
 (deftest cas-index-test
-  (let [idx (:createdIndex (:node (v/reset! c "foo" 0)))]
-    (v/cas-index! c :foo idx 1)
-    (is (= "1" (v/get c :foo)))
+  (let [idx (:createdIndex (:node (h/reset! c "foo" 0)))]
+    (h/cas-index! c :foo idx 1)
+    (is (= "1" (h/get c :foo)))
 
-    (is (false? (v/cas-index! c :foo idx 2)))
-    (is (= "1" (v/get c :foo)))))
+    (is (false? (h/cas-index! c :foo idx 2)))
+    (is (= "1" (h/get c :foo)))))
 
 (deftest swap-test
-  (v/reset! c :atom 0)
-  (is (= 3 (v/swap! c :atom (fn [a b c]
+  (h/reset! c :atom 0)
+  (is (= 3 (h/swap! c :atom (fn [a b c]
                               (+ (read-string a) b c)) 1 2)))
-  (is (= "3" (v/get c :atom))))
+  (is (= "3" (h/get c :atom))))
 
 ;(deftest swap-aggressive-test
-;  (v/reset! c :atom "0")
+;  (h/reset! c :atom "0")
 ;  (->> (range 100)
-;       (map #(future % (v/swap! c :atom (comp inc read-string))))
+;       (map #(future % (h/swap! c :atom (comp inc read-string))))
 ;       doall
 ;       (map deref)
 ;       sort
 ;       (= (range 1 101))
 ;       is)
-;  (is (= "100" (v/get c :atom {:consistent? true}))))
+;  (is (= "100" (h/get c :atom {:consistent? true}))))
